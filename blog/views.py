e7c511a449a88b854704bf497234be4d60068750
thundeyy <doncasino14@gmail.com>
2018-11-13 03:56:41 +0100

    * Added templating

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils import timezone
+
+from blog.models import Post
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    all_posts = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    return render(request, 'blog/post_list.html', {'posts': posts, 'posts_all': all_posts})
