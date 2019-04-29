f2b6f04fc56091f93c08566560a19c7cde6d18a8
Hassan Hasan (C) <hassan@eroeconsulting.com>
2019-04-29 15:26:51 +0100

    * Changed the logic for using the default login and log out views

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views
 from django.urls import path, include
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls'))
 ]
