d7e379dc631ee143a07936f5be39bf8874d8e7d2
Babatunde Hassan <doncasino14@gmail.com>
2019-04-29 13:52:54 +0100

    * Freezing requirements

--- blog/forms.py
+++ blog/forms.py
@@ -4,15 +4,12 @@ from blog.models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
-
     class Meta:
         model = Post
         fields = ('title', 'text')
 
 
-
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = Comment
         fields = ('author', 'text',)
